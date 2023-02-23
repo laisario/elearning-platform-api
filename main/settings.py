895917e299b203fb97202bd56117ac72760c0b37
Lucas Procopio <lp@tsl.io>
2023-02-23 14:55:05 -0300

    aaa

--- main/settings.py
+++ main/settings.py
@@ -36,6 +36,13 @@ ALLOWED_HOSTS = [
     "127.0.0.1"
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://medcursos.online",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://0.0.0.0:3000",
+]
+
 # Application definition
 
 INSTALLED_APPS = [
